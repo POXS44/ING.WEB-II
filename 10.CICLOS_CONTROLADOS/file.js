46f0722f8a6494eeb31a90bb525b21c5732f48dc
POXS44 <vzeledon7@gmail.com>
2021-02-12 12:08:41 -0600

    10.CICLOS_CONTROLADOS

---
+++ 10.CICLOS_CONTROLADOS/file.js
@@ -0,0 +1,12 @@
+
+
+let i = 0;
+for (;i<12;i++){
+   if(i===8){
+     break;
+   }
+}
+
+console.log(i);
+ 
+//8
---
+++ 10.CICLOS_CONTROLADOS/file1.js
@@ -0,0 +1,10 @@
+
+
+for (let i=0;i<4;i++){
+    if(i===2){
+      continue;
+    }
+    console.log(i);
+ }
+ 
+ // 0 1 3
