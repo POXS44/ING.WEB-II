b26499c26a2f0b0264a53e6d2f209fae58c821e3
POXS44 <vzeledon7@gmail.com>
2021-02-12 11:56:45 -0600

    TYPEOF()

---
+++ 8.TYPEOF_/file.js
@@ -0,0 +1,11 @@
+
+
+
+typeof(1); 	// number
+typeof(true); // boolean
+typeof('Hola'); // string
+typeof(function(){}); 	// function
+typeof({}); 	// object
+typeof(null); 	// object
+typeof(undefined); 	// undefined
+typeof(NaN); 	// number
