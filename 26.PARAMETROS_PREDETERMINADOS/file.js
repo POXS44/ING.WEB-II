d99eddf456d69892bd1f21da3130d4e9999930c3
POXS44 <vzeledon7@gmail.com>
2021-02-13 10:01:51 -0600

    26.PARAMETROS_PREDETERMINADOS

---
+++ 26.PARAMETROS_PREDETERMINADOS/file.js
@@ -0,0 +1,9 @@
+let camion = function(id,ciudad='NY'){
+    console.log(`Camion ${id} en ${ciudad}`);
+};
+
+console.log( camion(123) ); 
+// Camion 123 en NY
+
+console.log( camion(123, 'Chicago') ); 
+// Camion 123 en Chicago
