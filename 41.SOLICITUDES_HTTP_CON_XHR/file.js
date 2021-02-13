df7a3b12848d97469e320b9c163bb956caa4fdf9
POXS44 <vzeledon7@gmail.com>
2021-02-13 10:53:55 -0600

    41.SOLICITUDES_HTTP_CON_XHR

---
+++ 41.SOLICITUDES_HTTP_CON_XHR/file.js
@@ -0,0 +1,9 @@
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){          
+  if(this.readyState==4 && this.status==200){
+      console.log(this.responseText);
+  }
+);
+
+xhttp.open("GET",http://myid.mockapi.io/api/v1/users,true);
+xhttp.send();
