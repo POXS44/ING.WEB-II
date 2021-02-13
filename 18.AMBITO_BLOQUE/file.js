c91520c97f76f7459327fdc7cb60b81c1d79ef15
POXS44 <vzeledon7@gmail.com>
2021-02-13 09:05:08 -0600

    18.AMBITO_BLOQUE

---
+++ 18.AMBITO_BLOQUE/file.js
@@ -0,0 +1,5 @@
+if (5 == 5){ 
+    let mensaje = 'Igual...';
+ } 
+ 
+ console.log(mensaje); // error
---
+++ 18.AMBITO_BLOQUE/file1.js
@@ -0,0 +1,10 @@
+
+let mensaje = 'Fuera...';
+if (5 == 5){ 
+   let mensaje = 'Igual...';
+   console.log(mensaje); // Igual
+} 
+
+
+
+console.log(mensaje); // Fuera
